4b4c18b90d9f3ef8042049eab8fd444b5aa65cb9
Alex Votry <youremail@gmail.com>
2021-02-19 11:35:24 -0800

    hard code url!

--- src/service/socketConnection.js
+++ src/service/socketConnection.js
@@ -2,7 +2,7 @@ import io from 'socket.io-client';
 import { localURL } from './config';
 
 const url = process.env.SERVER_URL || localURL
-let socket = io.connect(url);
-console.log(url);
-
+console.log('url:', url, 'SERVER_URL:', process.env.SERVER_URL);
+const serverURL = process.env.NODE_ENV === 'production' ? 'https://scattegory-server.herokuapp.com' : 'http://localhost:3000';
+let socket = io.connect(serverURL);
 export default socket;
