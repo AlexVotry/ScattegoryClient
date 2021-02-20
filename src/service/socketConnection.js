d54b343db4a000ae7dae459aa4c78555987f3628
Alex Votry <youremail@gmail.com>
2021-02-19 16:51:57 -0800

    process.env is not correct, used window.location.href instead

--- src/service/socketConnection.js
+++ src/service/socketConnection.js
@@ -1,8 +1,8 @@
 import io from 'socket.io-client';
 import { localURL } from './config';
 
-const url = process.env.SERVER_URL || localURL
-console.log('url:', url, 'SERVER_URL:', process.env.SERVER_URL);
-const serverURL = process.env.NODE_ENV === 'production' ? 'https://scattegory-server.herokuapp.com' : 'http://localhost:3000';
+const url = window.location.href;
+console.log('url:', url,);
+const serverURL = url.includes('localhost') ? 'http://localhost:3000' : 'https://scattegory-server.herokuapp.com';
 let socket = io.connect(serverURL);
 export default socket;
